ba37c22fbc378d6c35837412e7f9bded87110b92
Devmustroc <elmustapha.abourar@outlook.com>
2023-08-27 14:03:13 +0200

    Typescript task 5 main.ts

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -1,37 +1,30 @@
-interface MajorCredits {
-    brand: "Major";
+export interface MajorCredits {
+    _MajorCreditBrand: void;
     credits: number;
 }
 
-interface MinorCredits {
-    brand: "Minor";
+export interface MinorCredits {
+    _majorCreditBrand: void;
     credits: number;
 }
 
-function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-    return {
-        brand: "Major",
-        credits: subject1.credits + subject2.credits,
-    };
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    return { credits: subject1.credits + subject2.credits } as MajorCredits;
 }
 
-function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-    return {
-        brand: "Minor",
-        credits: subject1.credits + subject2.credits,
-    };
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    return { credits: subject1.credits + subject2.credits } as MinorCredits;
 }
 
 // Test cases
-const majorSubject1: MajorCredits = { brand: "Major", credits: 3 };
-const majorSubject2: MajorCredits = { brand: "Major", credits: 4 };
+const majorSubject1: MajorCredits = { _MajorCreditBrand: undefined, credits: 3 };
+const majorSubject2: MajorCredits = { _MajorCreditBrand: undefined, credits: 4 };
 
-const minorSubject1: MinorCredits = { brand: "Minor", credits: 2 };
-const minorSubject2: MinorCredits = { brand: "Minor", credits: 1 };
+const minorSubject1: MinorCredits = { _majorCreditBrand: undefined, credits: 2 };
+const minorSubject2: MinorCredits = { _majorCreditBrand: undefined, credits: 1 };
 
 const sumOfMajorCredits = sumMajorCredits(majorSubject1, majorSubject2);
 console.log("Sum of Major Credits:", sumOfMajorCredits);
 
 const sumOfMinorCredits = sumMinorCredits(minorSubject1, minorSubject2);
 console.log("Sum of Minor Credits:", sumOfMinorCredits);
-
