844defe400ed38a240a9cc23b224e22acbe0ac1f
Devmustroc <elmustapha.abourar@outlook.com>
2023-08-28 15:19:40 +0200

    Jquery and install webpack

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,22 +1,17 @@
 import $ from 'jquery';
-import debounce from 'lodash/debounce';
+import _ from 'lodash';
 
-$(document).ready(function() {
-    const countParagraph = $('#count');
-    let count = 0;
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
 
-    function updateCounter() {
-        count++;
-        countParagraph.text(`${count} clicks on the button`);
-    }
+let count = 0;
 
-    const debouncedUpdateCounter = debounce(updateCounter, 300);
+$('button').on("click", _.debounce(updateCounter, 500))
 
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<button id="startButton">Click here to get started</button>');
-    $('body').append('<p id="count"></p>');
-    $('body').append('<p>Copyright - Holberton School</p>');
-
-    $('#startButton').on('click', debouncedUpdateCounter);
-});
+function updateCounter() {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+}
