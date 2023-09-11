fedf01bab8e4b3bf7a48dd470e82151b38073b46
Devmustroc <elmustapha.abourar@outlook.com>
2023-09-11 16:31:18 +0200

    :fire: first task_1

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -10,71 +10,52 @@ import './App.css';
 
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoggedIn: this.props.isLoggedIn
-        };
-    }
-
-    componentDidMount() {
-        window.addEventListener('keydown', (event) => {
-            if (event.ctrKey && event.key === 'h') {
-                alert('Logging you out');
-                this.props.logOut();
-            }
-        });
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('keydown', (event) => {});
-    }
-
     render() {
-      const {isLogged} = this.props;
-      const footerCopyright = `${getFooterCopy(true)} - ${getFullYear()}`;
+        const {isLoggedIn} = this.props;
+        this.isLoggedIn = true;
 
-      const listCourses = [
-            {id: 1, name: 'ES6', credit: 60},
-            {id: 2, name: 'Webpack', credit: 20},
-            {id: 3, name: 'React', credit: 40}
-      ];
-      const listNotifications = [
-            {id: 1, type: 'default', value: 'New course available'},
-            {id: 2, type: 'urgent', value: 'New resume available'},
-            {id: 3, type: 'urgent', html: {__html: getLatestNotification()}}
-      ];
-      return (
-          <>
-              <div className="App">
-                  <div className="header-wrapper">
-                      <div className="App-header">
-                          <Header />
-                      </div>
-                      <Notification listNotification={listCourses}/>
-                  </div>
-                  <div className="line"></div>
-                  <div className="App-body">
-                      {this.isLoggedIn ? <CourseList listCourse={listCourses}/> : <Login />}
-                  </div>
-                  <div className="line"></div>
-                  <div className="App-footer">
-                      <Footer text={footerCopyright}/>
-                  </div>
-              </div>
-          </>
-      )
-  }
+        const footerCopyright = `${getFooterCopy(true)} - ${getFullYear()}`
+
+        const listCourses = [
+            {id: 1, name: 'ES6', credit: '60'},
+            {id: 2, name: 'Webpack', credit: '20'},
+            {id: 3, name: 'React', credit: '40'}
+        ]
+        const listNotifications = [
+            {id: 1, type: "default", value: "New course available"},
+            {id: 2, type: "urgent", value: "New resume available"},
+            {id: 3, html: {__html: getLatestNotification()}, type: "urgent"}
+        ];
+
+        return (
+            <>
+                <div className="App">
+                    <div className="header-wrapper">
+                        <div className="App-header">
+                            <Header />
+                        </div>
+                        <Notification />
+                    </div>
+                    <div className="line"></div>
+                    <div className="App-body">
+                        {this.isLoggedIn ? <CourseList listCourse={listCourses}/> : <Login />}
+                    </div>
+                    <div className="line"></div>
+                    <div className="App-footer">
+                        <Footer text={footerCopyright}/>
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
 
 App.defaultProps = {
-    isLoggedIn: true,
-    logOut: () => {},
+    isLoggedIn: false
 };
 
 App.defaultProps = {
-    isLoggedIn: PropTypes.bool,
-    logOut: PropTypes.func,
+    isLoggedIn: PropTypes.bool
 };
 
 export default App;
--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -1,58 +1,85 @@
-import React, { Component } from 'react';
-import {getFooterCopy, getFullYear, getLatestNotification} from "../utils/utils";
-import Login from "../Login/Login";
-import CourseList from "../CourseList/CourseList";
+import React, { Fragment } from 'react';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+import { getLatestNotification } from '../utils/utils';
+import Login from '../Login/Login';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
 import Notification from "../Notifications/Notifications";
-import Footer from "../Footer/Footer";
 import PropTypes from "prop-types";
-import Header from "../Header/Header";
 import './App.css';
 
+const listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2,name: 'Webpack',credit: 20},
+    {id: 3,name: 'React',credit: 40}
+];
 
-class App extends Component {
-  render() {
-      const {isLogged} = this.props;
-      const footerCopyright = `${getFooterCopy(true)} - ${getFullYear()}`
-      const listCourses = [
-          {id: 1, name: 'ES6', credit: '60'},
-          {id: 2, name: 'Webpack', credit: '20'},
-          {id: 3, name: 'React', credit: '40'}
-      ]
-      const listNotifications = [
-          {id: 1, type: "default", value: "New course available"},
-          {id: 2, type: "urgent", value: "New resume available"},
-          {id: 3, html: {__html: getLatestNotification()}, type: "urgent"}
-      ];
+const listNotifications = [
+    { id: 1,type: 'default',value: 'New course available' },
+    { id: 2,type: 'urgent',value: 'New resume available' },
+    { id: 3,type: 'urgent',html: {__html: getLatestNotification()}}
+];
 
-      return (
-          <>
-              <div className="App">
-                  <div className="header-wrapper">
-                      <div className="App-header">
-                          <Header />
-                      </div>
-                      <Notification />
-                  </div>
-                  <div className="line"></div>
-                  <div className="App-body">
-                      {this.isLoggedIn ? <CourseList listCourse={listCourses}/> : <Login />}
-                  </div>
-                  <div className="line"></div>
-                  <div className="App-footer">
-                      <Footer text={footerCopyright}/>
-                  </div>
-              </div>
-          </>
-      )
-  }
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleKey = this.handleKey.bind(this);
+    }
+
+    handleKey(event) {
+        const isCtrl = event.ctrlKey;
+
+        if (isCtrl && event.key === 'h') {
+            event.preventDefault();
+            alert('Logging you out');
+            this.props.logOut();
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKey);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKey);
+    }
+
+    render() {
+        const footerCopyright = `${getFullYear()} - ${getFooterCopy(true)}`;
+        const { isLoggedIn } = this.props;
+
+        return (
+            <>
+                <div className="App">
+                    <div className="header-wrapper">
+                        <div className="App-header">
+                            <Header />
+                        </div>
+                        <Notification />
+                    </div>
+                    <div className="line"></div>
+                    <div className="App-body">
+                        {this.isLoggedIn ? <CourseList listCourse={listCourses}/> : <Login />}
+                    </div>
+                    <div className="line"></div>
+                    <div className="App-footer">
+                        <Footer text={footerCopyright}/>
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
 
-App.defaultProps = {
-    isLoggedIn: false
+App.propTypes = {
+    isLoggedIn: PropTypes.bool,
+    logOut: PropTypes.func
 };
 
 App.defaultProps = {
-    isLoggedIn: PropTypes.bool
+    isLoggedIn: false,
+    logOut: () => {}
 };
 
 export default App;
