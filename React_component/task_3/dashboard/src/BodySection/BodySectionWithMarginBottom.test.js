92b69c2a7896d0e027845e8e27637f8a4cbbba96
Devmustroc <elmustapha.abourar@outlook.com>
2023-09-12 10:40:36 +0200

    :fire: first task_3 BodySectionWithMarginBottom.test.js

---
+++ React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -0,0 +1,12 @@
+import { shallow, mount } from '../../config/setupTests';
+import React from 'react';
+import BodySection from './BodySection';
+import BodySectionWithMarginBottom from './BodySectionWithMarginBottom';
+
+describe('BodySectionWithMarginBottom', () => {
+    it ('Checks that component correctly renders a <BodySection /> component', () => {
+        const wrapper = shallow(<BodySectionWithMarginBottom title='test title'><p>test children node</p></BodySectionWithMarginBottom>);
+        expect(wrapper.find(BodySection)).props().title.toBe('test title');
+        expect(wrapper.find('p').text()).toBe('test children node');
+    });
+});
