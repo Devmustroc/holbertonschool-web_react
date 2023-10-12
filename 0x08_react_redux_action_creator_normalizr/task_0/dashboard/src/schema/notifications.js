24088960dff4e07d677e279db996bf2eae073c71
Devmustroc <elmustapha.abourar@outlook.com>
2023-10-12 13:13:14 +0200

    new

--- 0x08_react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++
@@ -1,7 +0,0 @@
-import * as notificationData from "../../dist/notifications";
-
-const getAllNotificationsByUser = (userId) => {
-	return notificationData.default.filter((notification) => notification.author.id === userId);
-};
-
-export default getAllNotificationsByUser;
