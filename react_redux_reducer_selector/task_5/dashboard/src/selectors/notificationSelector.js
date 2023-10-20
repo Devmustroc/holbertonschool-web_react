338df10fbf7a448293841d1fe475d7f51e741889
Devmustroc <elmustapha.abourar@outlook.com>
2023-10-20 15:48:11 +0200

    :memo: 4. Normalizr & Immutable

---
+++ react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.js
@@ -0,0 +1,7 @@
+const getNotifications = (state) => state.get("notifications");
+const getUnreadNotifications = (state) => {
+	return state.get("notifications").filter((notification) => notification.get("isRead") === false);
+}
+const filterTypeSelected = (state) => state.get("filter");
+
+export { getNotifications, getUnreadNotifications, filterTypeSelected };
