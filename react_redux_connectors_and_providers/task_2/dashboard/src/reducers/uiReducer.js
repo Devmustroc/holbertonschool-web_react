6d50ade8fd89b7936b893df7df9299aafdbb20cf
Devmustroc <elmustapha.abourar@outlook.com>
2023-10-23 12:05:39 +0200

    Modify the uiReducer

--- react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -1,41 +1,37 @@
-import { SELECT_COURSE, UNSELECT_COURSE} from "../actions/courseActionTypes";
-import { MARK_AS_READ, SET_TYPE_FILTER} from "../actions/notificationActionTypes";
 import {
-	  LOGIN,
-	  LOGOUT,
-	  LOGIN_SUCCESS,
-	  LOGIN_FAILURE,
-	  DISPLAY_NOTIFICATION_DRAWER,
-	  HIDE_NOTIFICATION_DRAWER
+	LOGIN,
+	LOGOUT,
+	LOGIN_SUCCESS,
+	LOGIN_FAILURE,
+	DISPLAY_NOTIFICATION_DRAWER,
+	HIDE_NOTIFICATION_DRAWER
 } from "../actions/uiActionTypes";
+import { Map } from 'immutable';
 
-export function uiReducer(state = appInitialState, action) {
+export const appInitialState = {
+	isNotificationDrawerVisible: false,
+	isUserLoggedIn: false,
+	user: null
+};
+
+export function uiReducer(state = Map(appInitialState), action) {
 	switch (action.type) {
 		case DISPLAY_NOTIFICATION_DRAWER:
-			return {
-				...state,
-				isNotificationDrawerVisible: true
-			};
+			return state.set('isNotificationDrawerVisible', true);
 		case HIDE_NOTIFICATION_DRAWER:
-			return {
-				...state,
-				isNotificationDrawerVisible: false
-			};
-		case LOGIN_SUCCESS:
-			return {
-				...state,
-				isUserLoggedIn: true
-			};
-		case LOGIN_FAILURE:
-			return {
-				...state,
-				isUserLoggedIn: false
-			};
+			return state.set('isNotificationDrawerVisible', false);
 		case LOGIN:
-			return {
-				...state,
-				isUserLoggingIn: true
-			};
+			return state.set('user', action.user);
+		case LOGIN_SUCCESS:
+			return state.set('isUserLoggedIn', true);
+		case LOGIN_FAILURE:
+			return state.set('isUserLoggedIn', false);
+		case LOGOUT:
+			const updatedState = state.withMutations((state) => {
+				state.set('isUserLoggedIn', false),
+					state.set('user', null)
+			});
+			return updatedState;
 		default:
 			return state;
 	}
