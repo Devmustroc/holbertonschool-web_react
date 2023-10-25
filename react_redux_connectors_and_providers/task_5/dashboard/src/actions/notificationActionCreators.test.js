b86c73effc6750f313693a11b625c89f31893600
Devmustroc <elmustapha.abourar@outlook.com>
2023-10-25 10:18:25 +0200

    :fire: Connect notifications: update the test suites

--- react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.test.js
+++ react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.test.js
@@ -1,33 +1,80 @@
 import {
-  markAsRead,
-  setNotificationFilter
-} from './notificationActionCreators';
+  MARK_AS_READ,
+  SET_TYPE_FILTER,
+  SET_LOADING_STATE,
+  FETCH_NOTIFICATIONS_SUCCESS,
+  NotificationTypeFilters
+} from "./notificationActionTypes";
+import {
+  markAsAread,
+  setNotificationFilter,
+  setLoadingState,
+  setNotifications,
+  fetchNotifications
+} from "./notificationActionCreators";
+import configureMockStore from 'redux-mock-store';
+import fetchMock from 'fetch-mock';
+import thunk from 'redux-thunk';
 
+const mockStore = configureMockStore([thunk]);
 
-describe('markAsRead', () => {
-  it(`Tests that markAsRead's dispatch is called with the right type and index`, () => {
-    const dispatch = jest.fn();
-    const index = 1;
+describe('Notification Action creators tests', () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+  it('Tests the login action', () => {
+    expect(markAsAread(1)).toEqual({ type: MARK_AS_READ, index: 1 });
+  });
+  it('Tests the logout action', () => {
+    const defaultAction = {
+      type: SET_TYPE_FILTER,
+      filter: 'DEFAULT'
+    }
+    const urgentAction = {
+      type: SET_TYPE_FILTER,
+      filter: 'URGENT'
+    }
+    expect(setNotificationFilter(NotificationTypeFilters.DEFAULT)).toEqual(defaultAction);
+    expect(setNotificationFilter(NotificationTypeFilters.URGENT)).toEqual(urgentAction);
+  });
+  it('Tests the setLoadingState action', () => {
+    const action = {
+      type: SET_LOADING_STATE,
+      loading: true
+    };
+    expect(setLoadingState(true)).toEqual(action);
+  });
+  it('Tests the setNotifications action', () => {
+    const action = {
+      type: FETCH_NOTIFICATIONS_SUCCESS,
+      data: {}
+    };
+    expect(setNotifications({})).toEqual(action);
+  });
+  it('Tests the fetchNotifications action', () => {
+    const store = mockStore({});
+    const expectedActions = [
+      {
+        type: SET_LOADING_STATE,
+        loading: true
+      },
+      {
+        type: FETCH_NOTIFICATIONS_SUCCESS,
+        data: []
+      },
+      {
+        type: SET_LOADING_STATE,
+        loading: false
+      }
+    ];
 
-    markAsRead(index)(dispatch);
+    fetchMock.get('http://localhost:8564/notifications.json', {
+      status: 200,
+      body: [],
+    });
 
-    expect(dispatch).toHaveBeenCalledWith({
-      type: 'MARK_AS_READ',
-      index
+    return store.dispatch(fetchNotifications()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
     });
   });
-})
-
-describe('setNotificationFilter', () => {
-  it(`Tests that setNotificationFilter's dispatch is called with the right type and filter`, () => {
-    const dispatch = jest.fn();
-    const filter = 'all';
-
-    setNotificationFilter(filter)(dispatch);
-
-    expect(dispatch).toHaveBeenCalledWith({
-      type: 'SET_TYPE_FILTER',
-      filter
-    });
-  });
-})
+});
